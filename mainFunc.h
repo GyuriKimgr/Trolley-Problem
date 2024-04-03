34de8a6204d0dfea210489d24b257d30c99db878
GyuriKimgr <129020605+GyuriKimgr@users.noreply.github.com>
2024-04-04 07:26:26 +0900

    Add Trolley.c And mainFunc.h

---
+++ mainFunc.h
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <string.h>
+
+#pragma once
+
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+#define SUBMIT 4
+
+void hideCursor() { //깜박이는 커서 숨김
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO ConsoleCursor;
+    ConsoleCursor.bVisible = 0;
+    ConsoleCursor.dwSize = 1;
+    SetConsoleCursorInfo(consoleHandle, &ConsoleCursor);
+}
+void gotoxy(int x, int y) { //x, y 좌표 설정
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos;
+    pos.X = x;
+    pos.Y = y;
+    SetConsoleCursorPosition(consoleHandle, pos);
+}
+int keyControl()//키보드 이벤트 처리
+{
+    char temp = _getch();//키보드 입력
+    if (temp == 'w' || temp == 'W') return UP;
+    else if (temp == 'a' || temp == 'A') return LEFT;
+    else if (temp == 's' || temp == 'S') return DOWN;
+    else if (temp == 'd' || temp == 'D') return RIGHT;
+    else if (temp == ' ') return SUBMIT;
+}
+int drawMenu() {
+    int x = 35, y = 20; //메뉴 위치 설정
+    printf("\n\n\n");
+	printf("++===========================================================================++\n");
+	printf("++===========================================================================++\n");
+	printf(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
+	printf("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-\n");
+	printf(" [][][][][]                          []    [] [][][]\n");
+	printf(" [][][][][]              [] [] [][][] []  []  []  []                   [][][]\n");
+	printf("     []    []     [][][] [] [] []      [][]   []         []            []    \n");
+	printf("     []    []  [] []  [] [] [] []       []    []        [][]  [][][][] []  \n");
+	printf("     []    [][]   []  [] [] [] [][][]   []    []  [][] []  [] [] [] [] [][][]\n");
+	printf("     []    []     []  [] [] [] []       []    []  []   [][][] [] [] [] []\n");
+	printf("     []    []     [][][] [] [] [][][]   []    [][][]   []  [] [] [] [] [][][]\n");
+	printf("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-\n");
+	printf(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
+	printf("++===========================================================================++\n");
+	printf("++===========================================================================++\n");
+	printf("                           >>CH00$3 TH3 M3NU<<                                   \n");
+	gotoxy(x - 2, y);
+	printf("> PLAY\n");
+	gotoxy(x, y + 1);
+	printf("QU1T\n");
+
+    while (1)
+    {
+        int n = keyControl();//키보드 입력
+        switch (n)
+        {
+        case UP: //메뉴 위아래 범위 지정
+            if (y > 20)
+            {
+                gotoxy(x - 2, y);
+                printf(" ");
+                gotoxy(x - 2, --y);
+                printf(">");
+            }
+            break;
+
+        case DOWN:
+            if (y < 21)
+            {
+                gotoxy(x - 2, y);
+                printf(" ");
+                gotoxy(x - 2, ++y);
+                printf(">");
+            }
+            break;
+
+        case SUBMIT:
+            return y - 16;// 0, 1, 2 반환
+        }
+    }
+}
+int track() {
+    printf("                                        |---|---|---|---|---|---|---|---|---|\n");
+    printf("                                    |---|---|\n");
+    printf("|---|---|---|---|---|---|---|---|---|---|\n");
+    printf("                                    |---|---|\n");
+    printf("                                        |---|---|---|---|---|---|---|---|---|\n");
+}
+int trolley() {
+    printf(" ______________\n");
+    printf("|▣▣▣▣▣▣▣|\n");
+    printf(" _◎◎◎__◎◎◎\n");
+}
