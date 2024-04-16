6ce1565e552606123a80755668d438e317860025
gyuri <takaneene1211@naver.com>
2024-04-16 11:41:57 +0900

    Modify questions

--- Trolley Problem/mainFunc.h
+++ Trolley Problem/mainFunc.h
@@ -30,9 +30,32 @@ void gotoxy(int x, int y) { //x, y 좌표 설정
     COORD pos = {x, y};
     SetConsoleCursorPosition(consoleHandle, pos);
 }
-void printCountdown(int seconds) { //카운트다운 출력
-    printf("\r%d seconds ", seconds); //공백으로 덮어쓰기
-    fflush(stdout); //출력버퍼 지우기
+void printCountdown(int seconds) { // 카운트다운 출력
+    printf("\r%d seconds ", seconds); // 공백으로 덮어쓰기
+    fflush(stdout); // 출력버퍼 지우기
+}
+void timerLoop() {
+    time_t sTime, cTime;
+    int c = 1;
+    while (c <= 10) {
+        sTime = time(NULL) + 10;
+        int timerE = 0;
+        do { // timer
+            cTime = time(NULL);
+            if (sTime - cTime >= 0) {
+                printCountdown(sTime - cTime);
+                Sleep(1000);
+            }
+            else {
+                timerE = 1;
+                break;
+            }
+        } while (1);
+
+        if (timerE) {
+            c++;
+        }
+    }
 }
 
 int keyControl()//키보드 이벤트 처리
@@ -123,6 +146,7 @@ void HowtoPlay() { //게임정보출력
     }
 }
 int question_1() {
+    gotoxy(0, 0);
     int x = 0, y = 15; // 초기 위치 설정
     gotoxy(x + 54, 7);
     printf("●\n");
@@ -135,6 +159,7 @@ int question_1() {
         printf("↑ Abandoned baby\n");
         gotoxy(x + 25, 17);
         printf("↓ Abandoned elderly person\n");
+
 
         // 현재 위치에 따라 색상 변경
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
@@ -154,114 +179,340 @@ int question_1() {
         else if (temp == 's' || temp == 'S') {
             if (y < 17) y++;
         }
+        else if (temp == ' ') {
+            break;
+        }
     }
 }
 
-
-    
-    
 int question_2() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("●\n");
-    gotoxy(x + 54, y + 11);
+    gotoxy(x + 54, 11);
     printf("○\n");
-    gotoxy(x + 10, y + 15);
-    printf("↑ A man who has rap sheets but who can doing 10 people's job\n");
-    gotoxy(x + 10, y + 17);
-    printf("↓ A man who can't doing even 1 person's job\n");
+    while (1) {
+        gotoxy(x + 10, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ A man who has rap sheets but who can doing 10 people's job\n");
+        gotoxy(x + 10, 17);
+        printf("↓ A man who can't doing even 1 person's job\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 10, 15);
+            printf("↑ A man who has rap sheets but who can doing 10 people's job\n");
+        }
+        else {
+            gotoxy(x + 10, 17);
+            printf("↓ A man who can't doing even 1 person's job\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_3() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("○○\n");
-    gotoxy(x + 54, y + 11);
+    gotoxy(x + 54, 11);
     printf("○○\n");
-    gotoxy(x + 5, y + 15);
-    printf("↑ Poor white parents placed their child in an orphanage with heavy heart\n");
-    gotoxy(x + 5, y + 17);
-    printf("↓ Wealthy Asian parent who places excessive pressure on their child, \n       causing stress\n");
+    while (1) {
+        gotoxy(x + 5, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ Poor white parents placed their child in an orphanage with heavy heart\n");
+        gotoxy(x + 5, 17);
+        printf("↓ Wealthy Asian parent who places excessive pressure on their child, \n\tcausing stress\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 5, 15);
+            printf("↑ Poor white parents placed their child in an orphanage with heavy heart\n");
+        }
+        else {
+            gotoxy(x + 5, 17);
+            printf("↓ Wealthy Asian parent who places excessive pressure on their child, \n\tcausing stress\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_4() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("○\n");
-    gotoxy(x + 54, y + 11);
+    gotoxy(x + 54, 11);
     printf("○○○○○\n");
-    gotoxy(x + 30, y + 15);
-    printf("↑ A person\n");
-    gotoxy(x + 30, y + 17);
-    printf("↓ 5 people\n");
+    while (1) {
+        gotoxy(x + 30, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ A person\n");
+        gotoxy(x + 30, 17);
+        printf("↓ 5 people\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 30, 15);
+            printf("↑ A person\n");
+        }
+        else {
+            gotoxy(x + 30, 17);
+            printf("↓ 5 people\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_5() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("◐\n");
-    gotoxy(x + 54, y + 11);
+    gotoxy(x + 54, 11);
     printf("○○○○○\n");
-    gotoxy(x + 30, y + 15);
-    printf("↑ 1 disabled person\n");
-    gotoxy(x + 30, y + 17);
-    printf("↓ 5 healthy man\n");
+    while (1) {
+        gotoxy(x + 30, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ 1 disabled person\n");
+        gotoxy(x + 30, 17);
+        printf("↓ 5 healthy man\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 30, 15);
+            printf("↑ 1 disabled person\n");
+        }
+        else {
+            gotoxy(x + 30, 17);
+            printf("↓ 5 healthy man\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_6() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("●●●●●\n");
-    gotoxy(x + 54, y + 11);
+    gotoxy(x + 54, 11);
     printf("●\n");
-    gotoxy(x + 25, y + 15);
-    printf("↑ 5 corrupt politicians\n");
-    gotoxy(x + 25, y + 17);
-    printf("↓ 1 corrupt CEO\n");
+    while (1) {
+        gotoxy(x + 30, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ 5 corrupt politicians\n");
+        gotoxy(x + 30, 17);
+        printf("↓ 1 corrupt CEO\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 30, 15);
+            printf("↑ 5 corrupt politicians\n");
+        }
+        else {
+            gotoxy(x + 30, 17);
+            printf("↓ 1 corrupt CEO\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_7() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("♥\n");
-    gotoxy(x + 54, y + 11);
+    gotoxy(x + 54, 11);
     printf("▲▲▲▲▲\n");
-    gotoxy(x + 25, y + 15);
-    printf("↑ ♡♥AdOrAbLe kItTy♥♡ \n");
-    gotoxy(x + 25, y + 17);
-    printf("↓ 5 children\n");
+    while (1) {
+        gotoxy(x + 30, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ ♡♥AdOrAbLe kItTy♥♡ \n");
+        gotoxy(x + 30, 17);
+        printf("↓ 5 children\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 30, 15);
+            printf("↑ ♡♥AdOrAbLe kItTy♥♡ \n");
+        }
+        else {
+            gotoxy(x + 30, 17);
+            printf("↓ 5 children\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_8() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
-    printf("▮▮▮▮▮▮▮▮▮▮\n");
-    gotoxy(x + 54, y + 11);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
+    printf("##########\n");
+    gotoxy(x + 54, 11);
     printf("◎\n");
-    gotoxy(x + 25, y + 15);
-    printf("↑ 10 Beautiful Trees\n");
-    gotoxy(x + 25, y + 17);
-    printf("↓ Dead, Cold meats 22 pounds(10kg)\n");
+    while (1) {
+        gotoxy(x + 25, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ 10 Beautiful Trees\n");
+        gotoxy(x + 25, 17);
+        printf("↓ Dead, Cold meats 22 pounds(10kg)\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 25, 15);
+            printf("↑ 10 Beautiful Trees\n");
+        }
+        else {
+            gotoxy(x + 25, 17);
+            printf("↓ Dead, Cold meats 22 pounds(10kg)\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_9() {
-    int x = 0, y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("▣\n");
-    gotoxy(x + 54, y + 10);
+    gotoxy(x + 54, 11);
     printf("○○○○○\n");
-    gotoxy(x + 25, y + 15);
-    printf("↑ The only perfect quantum computer in the world\n");
-    gotoxy(x + 25, y + 17);
-    printf("↓ 5 hardworking developers\n");
+    while (1) {
+        gotoxy(x + 25, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ The only perfect quantum computer in the world\n");
+        gotoxy(x + 25, 17);
+        printf("↓ 5 hardworking developers\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 25, 15);
+            printf("↑ The only perfect quantum computer in the world\n");
+        }
+        else {
+            gotoxy(x + 25, 17);
+            printf("↓ 5 hardworking developers\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 int question_10() {
-    int x = 0 , y = 0;
-    gotoxy(x + 54, y + 7);
+    gotoxy(0, 0);
+    int x = 0, y = 15;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+    gotoxy(x + 54, 7);
     printf("○\n");
-    gotoxy(x + 54, y + 10);
+    gotoxy(x + 54, 11);
     printf("○\n");
-    gotoxy(x + 25, y + 15);
-    printf("↑ your old friend\n");
-    gotoxy(x + 25, y + 17);
-    printf("↓ your old friend's cousin\n");
+    while (1) {
+        gotoxy(x + 25, 15);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
+        printf("↑ your old friend\n");
+        gotoxy(x + 25, 17);
+        printf("↓ your old friend's cousin\n");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MINT_COLOR);
+        if (y == 15) {
+            gotoxy(x + 25, 15);
+            printf("↑ your old friend\n");
+        }
+        else {
+            gotoxy(x + 25, 17);
+            printf("↓ your old friend's cousin\n");
+        }
+        char temp = _getch();
+        if (temp == 'w' || temp == 'W') {
+            if (y > 15) y--;
+        }
+        else if (temp == 's' || temp == 'S') {
+            if (y < 17) y++;
+        }
+        else if (temp == ' ') {
+            break;
+        }
+    }
 }
 
 int track() {
     int x = 0, y = 8;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
     gotoxy(x, y);
     printf("                                        |---|---|---|---|---|---|---|---|---|\n");
     printf("                                    |---|---|\n");
@@ -271,74 +522,75 @@ int track() {
 }
 int trolley() {
     int x = 10, y = 7;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DEFALT_COLOR);
     gotoxy(x, y);
     printf(" ______________\n");
-    gotoxy(x, y+1);
+    gotoxy(x, y + 1);
     printf("|▣▣▣▣▣▣▣|\n");
-    gotoxy(x, y+2);
+    gotoxy(x, y + 2);
     printf(" _◎◎◎__◎◎◎\n");
 }
-void playGame() {
-    int x = 0, y = 0;
-    srand(time(NULL));
-    time_t sTime, cTime;
-    int c = 1;
-    int keyinput;
-    while (c <= 10) {
-        system("cls");
-        track();
-        trolley();
-        sTime = time(NULL) + 10;
-        int timerE = 0;
-        switch (c) {
-        case 1:
-            question_1();
-            break;
-        case 2:
-            question_2();
-            break;
-        case 3:
-            question_3();
-            break;
-        case 4:
-            question_4();
-            break;
-        case 5:
-            question_5();
-            break;
-        case 6:
-            question_6();
-            break;
-        case 7:
-            question_7();
-            break;
-        case 8:
-            question_8();
-            break;
-        case 9:
-            question_9();
-            break;
-        case 10:
-            question_10();
-            break;
-        }
-        do {//timer
-            cTime = time(NULL);
-            if (sTime - cTime >= 0) {
-                gotoxy(x, y);
-                printCountdown(sTime - cTime);
-                Sleep(1000);
-            }
-            else {
-                timerE = 1;
-                break;
-            }
-        } while (1);
 
-        if (timerE) {
-            c++;
+void playGame() {
+    srand(time(NULL));
+    int g = 1;
+    while (g <= 10) {
+        switch (g) {
+        case 1:
+            system("cls");
+            track();
+            trolley();
+            question_1();
+        case 2:
+            system("cls");
+            track();
+            trolley();
+            question_2();
+        case 3:
+            track();
+            trolley();
+            question_3();
+        case 4:
+            system("cls");
+            track();
+            trolley();
+            question_4();
+        case 5:
+            system("cls");
+            track();
+            trolley();
+            question_5();
+        case 6:
+            track();
+            trolley();
+            question_6();
+        case 7:
+            system("cls");
+            track();
+            trolley();
+            question_7();
+        case 8:
+            system("cls");
+            track();
+            trolley();
+            question_8();
+        case 9:
+            system("cls");
+            track();
+            trolley();
+            question_9();
+        case 10:
+            system("cls");
+            track();
+            trolley();
+            question_10();
         }
-       Sleep(1000);
-       system("cls");
+        while (1) {
+            if (keyControl() == SUBMIT) {
+                break;
+                g++;
+            }
+        }
+        break;
     }
 }
